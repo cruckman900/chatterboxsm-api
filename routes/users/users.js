bc687809f7c1de7183c5bb3481d89fe0d2be9b8d
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-04 00:18:21 -0400

    fixed sql calls.  removed console logs.

--- routes/users/users.js
+++ routes/users/users.js
@@ -4,77 +4,64 @@ const users = require('../../services/users/users');
 
 /* GET user */
 router.get('/', async function(req, res, next) {
-    try {
-        if (req.query.action === "getUserByID") {
-            return new Promise(function() {
-                users.getByID(req.query.id)
-                    .then(result => res.json(result))
-                    .catch(err => res.json(err));
-            })
-        } else if (req.query.action === "getUserByUsernameAndPassword") {
-            return new Promise(function() {
-                users.getByUsernameAndPassword(req.query.username, req.query.password)
-                    .then(result => res.json(result))
-                    .catch(err => res.json(err));
-            })
-        } else if (req.query.action === 'getCountUsers') {
-            return new Promise(function() {
-                users.getUserCount()
-                    .then(result => res.json(result))
-                    .catch(err => res.json(err));
-            })
-        } else if (req.query.action === 'getCountUsersOnline') {
-            return new Promise(function() {
-                users.getUserCountIsLoggedIn()
-                    .then(result => res.json(result))
-                    .catch(err => res.json(err));
-            })
-        } else {
-            return new Promise(function() {
-                users.getAll()
-                    .then(result => res.json(result))
-                    .catch(err => res.json(err));
-            })
-        }
-    } catch (err) {
-        console.log(`Error while getting user(s) `, err.message);
-        next(err);
+    if (req.query.action === "getUserByID") {
+        return new Promise(function() {
+            users.getByID(req.query.id)
+                .then(result => res.json(result))
+                .catch(err => res.json(err));
+        })
+    } else if (req.query.action === "getUserByUsernameAndPassword") {
+        return new Promise(function() {
+            users.getByUsernameAndPassword(req.query.username, req.query.password)
+                .then(result => res.json(result))
+                .catch(err => res.json(err));
+        })
+    } else if (req.query.action === 'getCountUsers') {
+        return new Promise(function() {
+            users.getUserCount()
+                .then(result => res.json(result))
+                .catch(err => res.json(err));
+        })
+    } else if (req.query.action === 'getCountUsersOnline') {
+        return new Promise(function() {
+            users.getUserCountIsLoggedIn()
+                .then(result => res.json(result))
+                .catch(err => res.json(err));
+        })
+    } else {
+        return new Promise(function() {
+            users.getAll()
+                .then(result => res.json(result))
+                .catch(err => res.json(err));
+        })
     }
 });
 
-/* POST users */
+/* POST user */
 router.post('/', async function(req, res, next) {
-    try {
-        console.log('POST user req.body', req.body);
-        const result = await users.create(req.body);
-        res.json(result);
-    } catch (err) {
-        console.error(`Error while creating users `, err.message);
-        next(err);
-    }
+    return new Promise(function() {
+        users.create(req.body)
+            .then(result => res.json(result))
+            .catch(err => res.json(err));
+    })
 });
 
 /* PUT user */
 router.put('/', async function(req, res, next) {
-    try {
-        console.log('PUT user req.body', req.body);
-        const result = await users.update(req.body);
-        res.json(result);
-    } catch (err) {
-        console.log(`Error while updating users `, err.message);
-        next(err);
-    }
+    return new Promise(function() {
+        users.update(req.body)
+            .then(result => res.json(result))
+            .catch(err => res.json(err));
+    })
 });
 
 /* DELETE user */
 router.delete('/:id', async function(req, res, next) {
-    try {
-        const result = await users.remove(req.params.id);
-        res.json(result);
-    } catch (err) {
-        console.error(`Error while deleting user `, err.message)
-        next(err);
-    }
+    return new Promise(function() {
+        users.remove(req.param.id)
+            .then(result => res.json(result))
+            .catch(err => res.json(err));
+    })
 });
 
 module.exports = router;
--- services/users/users.js
+++ services/users/users.js
@@ -2,21 +2,21 @@ const db = require('../db');
 
 const select = `SELECT u.id as USERID, u.firstname, u.middlename, u.lastname, u.screenname, 
     u.agerange, u.gender, u.description, u.verificationcode, u.validated, u.isLoggedIn, 
-    l.city, l.state, l.country, 
-    ss.id, ss.avatar, ss.primarycolor, ss.secondarycolor, 
-    act.id, act.archery_guns, act.arts_crafts, act.bars_clubs, act.boxing_wrestling, act.billiards_darts, 
+    l.id AS LID, l.city, l.state, l.country, 
+    ss.id AS SSID, ss.avatar, ss.primarycolor, ss.secondarycolor, 
+    act.id AS ACTID, act.archery_guns, act.arts_crafts, act.bars_clubs, act.boxing_wrestling, act.billiards_darts, 
     act.boating_camping, act.classicsports, act.cycling, act.fishing_hunting, act.hiking_climbing, 
     act.machines_electronics, act.martialarts, act.musicalinstruments ,act.puzzles_games, 
     act.reading_writing, act.singing_dancing, act.swimming, act.videogames, act.walking_running, 
     act.watchingtv, act.other as activities_other, 
-    f.id, f.american, f.asian_indian, f.cajun, f.hungarian, f.italian, f.mediterranean, f.latin_mexican, 
+    f.id AS FID, f.american, f.asian_indian, f.cajun, f.hungarian, f.italian, f.mediterranean, f.latin_mexican, 
     f.russian, f.middleeastern, f.slavic, f.romanian, f.other as foods_other, 
-    mov.id, mov.action, mov.comedy, mov.comics_animation, mov.documentary, mov.drama, mov.history, mov.mystery, 
+    mov.id AS MOVID, mov.action, mov.comedy, mov.comics_animation, mov.documentary, mov.drama, mov.history, mov.mystery, 
     mov.nature, mov.news_worldaffairs, mov.religion, mov.romance, mov.scifi, mov.sports, 
     mov.suspense_thriller, mov.other as movies_other, 
-    mus.id, mus.americanpop, mus.classical, mus.country_bluegrass, mus.flamenco_mariachi, mus.folk, mus.jazz, 
+    mus.id AS MUSID, mus.americanpop, mus.classical, mus.country_bluegrass, mus.flamenco_mariachi, mus.folk, mus.jazz, 
     mus.jpop_kpop, mus.metal, mus.polka, mus.rap_hiphop, mus.regae, mus.rock, mus.tribal, mus.other as music_other, 
-    tech.id, tech.digitalart_media, tech.gamedevelopment, tech.officesoftwareproficiency, tech.softwaredevelopment, 
+    tech.id AS TECHID, tech.digitalart_media, tech.gamedevelopment, tech.officesoftwareproficiency, tech.softwaredevelopment, 
     tech.technicalwriting, tech.other as tech_other 
     FROM ${process.env.dbdatabase}.users u 
         LEFT JOIN ${process.env.dbdatabase}.locations l ON u.id = l.userid 
@@ -28,7 +28,9 @@ const select = `SELECT u.id as USERID, u.firstname, u.middlename, u.lastname, u.
         LEFT JOIN ${process.env.dbdatabase}.technicalaptitude tech ON u.id = tech.userid`;
 
 
-function runQuery(sql) {
+async function getUserCount() {
+    const sql =`SELECT COUNT(*) FROM ${process.env.dbdatabase}.users`;
+
     return new Promise(function(resolve, reject) {
         db.query(sql)
             .then(result => resolve(result))
@@ -36,30 +38,45 @@ function runQuery(sql) {
     });
 }
 
-async function getUserCount() {
-    const sql =`SELECT COUNT(*) FROM ${process.env.dbdatabase}.users`;
-    return runQuery(sql);
-}
-
 async function getUserCountIsLoggedIn() {
     const sql = `SELECT COUNT(*) FROM ${process.env.dbdatabase}.users 
         WHERE isLoggedIn=true`;
-    return runQuery(sql);
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
 }
 
 async function getAll() {
     const sql = `SELECT * FROM users;`;
-    return runQuery(sql);
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
 }
 
 async function getByID(id) {
     const sql = `${select} WHERE u.id=${id};`;
-    return runQuery(sql);
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
 }
 
 async function getByUsernameAndPassword(username, password) {
     const sql = `${select} WHERE u.username="${username}" AND u.password="${password}";`;
-    return runQuery(sql);
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
 }
 
 async function create(user) {
@@ -70,7 +87,12 @@ async function create(user) {
     ("${user.data.firstname}", "${user.data.middlename}", "${user.data.lastname}", "${user.data.screenname}", 
     "${user.data.email}", "${user.data.agerange}", "${user.data.gender}", "${user.data.username}", "${user.data.password}", 
     "${user.data.description}", ${user.data.verificationcode}, ${user.data.validated});`;
-    return runQuery(sql);
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
 }
 
 async function update(user) {
@@ -80,14 +102,24 @@ async function update(user) {
     username="${user.data.username}", password="${user.data.password}", description="${user.data.description}", 
     verificationcode=${user.data.verificationcode}, validated=${user.data.validated}, isLoggedIn=${user.data.isLoggedIn} 
     WHERE username="${user.data.username}" AND password="${user.data.password}";`;
-    return runQuery(sql);
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
 }
 
 async function remove(id) {
     const result = await db.query(
         `DELETE FROM ${process.env.dbdatabase}.users WHERE id=${id};`
     );
-    return runQuery(sql);
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
 }
 
 module.exports = {
