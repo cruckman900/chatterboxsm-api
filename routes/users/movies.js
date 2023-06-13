aa77d33c77033958753b30243fb15cd25516123a
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-13 10:54:11 -0400

    added nodemon package.  added movies api

--- routes/users/movies.js
+++ routes/users/movies.js
@@ -1,18 +1,41 @@
-/*  movies
-    id:                     INT
-    userid:                 INT
-    action:                 TINYINT
-    comedy:                 TINYINT
-    comics_animation:       TINYINT
-    documentary:            TINYINT
-    drama:                  TINYINT
-    history:                TINYINT
-    mystery:                TINYINT
-    nature:                 TINYINT
-    news_worldaffairs:      TINYINT
-    religion:               TINYINT
-    scifi:                  TINYINT
-    sports:                 TINYINT
-    suspense_thriller:      TINYINT
-    other:                  TINYINT
-*/
+const express = require('express');
+const router = express.Router();
+const movies = require('../../services/users/movies');
+
+/* GET movies */
+router.get('/', async function(req, res, next) {
+    return new Promise(function() {
+        movies.getByUserID(req.query.userID)
+            .then(result => res.json(result))
+            .catch(err => res.json(err));
+    });
+});
+
+/* POST movies */
+router.post('/', async function(req, res, next) {
+    return new Promise(function() {
+        movies.create(req.body)
+            .then(result => res.json(result))
+            .catch(err => res.json(err));
+    });
+});
+
+/* PUT movies */
+router.put('/', async function(req, res, next) {
+    return new Promise(function() {
+        movies.update(req.body)
+            .then(result => res.json(result))
+            .catch(err => res.json(err));
+    });
+});
+
+/* DELETE movies */
+router.delete('/:id', async function(req, res, next) {
+    return new Promise(function() {
+        movies.remove(req.param.id)
+            .then(result => res.json(result))
+            .catch(err => res.json(err));
+    });
+});
+
+module.exports = router;
---
+++ services/users/movies.js
@@ -0,0 +1,68 @@
+const db = require('../db');
+
+const select = `SELECT id, userid, action, comedy, comics_animation, documentary, drama, 
+    history, mystery, nature, news_worldaffairs, religion, romance, scifi, sports, 
+    suspense_thriller, other `;
+
+async function getByUserID(id) {
+    const sql =`${select} WHERE userid=${id};`;
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
+}
+
+async function create(movies) {
+    var sql = `INSERT INTO ${process.env.dbdatabase}.movies 
+        (userid, action, comedy, comics_animation, documentary, drama, 
+        history, mystery, nature, news_worldaffairs, religion, romance, scifi, sports, 
+        suspense_thriller, other) 
+        VALUES
+        (${movies.data.userid}, ${movies.data.action}, ${movies.data.comedy}, ${movies.data.comics_animation}, 
+        ${movies.data.documentary}, ${movies.data.drama}, ${movies.data.history}, ${movies.data.mystery}, 
+        ${movies.data.nature}, ${movies.data.news_worldaffairs}, ${movies.data.religion}, 
+        ${movies.data.romance}, ${movies.data.scifi}, ${movies.data.sports}, 
+        ${movies.data.suspense_thriller}, ${movies.data.other});`;
+    
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
+}
+
+async function update(movies) {
+    var sql = `UPDATE ${process.env.dbdatabase}.movies 
+        SET userid=${movies.data.userid}, action=${movies.data.action}, comedy=${movies.data.comedy}, 
+        comics_animation=${movies.data.comics_animation}, documentary=${movies.data.documentary}, 
+        drama=${movies.data.drama}, history=${movies.data.history}, mystery=${movies.data.mystery}, 
+        nature=${movies.data.nature}, news_worldaffairs=${movies.data.news_worldaffairs}, religion=${movies.data.religion} 
+        romance=${movies.data.romance}, scifi=${movies.data.scifi}, sports=${movies.data.sports}, 
+        suspense_thriller=${movies.data.suspense_thriller}, other=${movies.data.other} 
+        WHERE userid=${movies.data.userid};`;
+    
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
+}
+
+async function remove(id) {
+    const sql = `DELETE FROM ${process.enf.dbdatabase}.movies WHERE userid=${id};`;
+
+    return new Promise(function(resolve, reject) {
+        db.query(sql)
+            .then(result => resolve(result))
+            .catch(err => reject(err));
+    });
+}
+
+module.exports = {
+    getByUserID,
+    create,
+    update,
+    remove
+}
