9d9efe4e4403d825326eca3d5e6bcc6b0b8a2d4c
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-06-16 04:02:24 -0400

    removed dupe columns in sql.  removed temporary return val.

--- services/users/foods.js
+++ services/users/foods.js
@@ -17,15 +17,13 @@ async function getByUserID(id) {
 async function create(foods) {
     const sql = `INSERT INTO ${process.env.dbdatabase}.foods 
         (userid, american, asian_indian, cajun, hungarian, italian, mediterranean, 
-        latin_mexican, russian, middleeastern, slavic, romanian, other, french, cookies, french, cookies) 
+        latin_mexican, russian, middleeastern, slavic, romanian, other, french, cookies) 
         VALUES 
         (${foods.data.userid}, ${foods.data.american}, ${foods.data.asian_indian}, 
         ${foods.data.cajun}, ${foods.data.hungarian}, ${foods.data.italian}, 
         ${foods.data.mediterranean}, ${foods.data.latin_mexican}, ${foods.data.russian}, 
         ${foods.data.middleeastern}, ${foods.data.slavic}, ${foods.data.romanian}, 
-        ${foods.data.other}, ${foods.data.french}, ${foods.data.cookies}, ${foods.data.french}, ${foods.data.cookies});`;
-
-    return {foods, sql}
+        ${foods.data.other}, ${foods.data.french}, ${foods.data.cookies});`;
 
     return new Promise(function(resolve, reject) {
         db.query(sql)
@@ -42,7 +40,7 @@ async function update(foods) {
         mediterranean=${foods.data.mediterranean}, latin_mexican=${foods.data.latin_mexican}, 
         russian=${foods.data.russian}, middleeastern=${foods.data.middleeastern}, 
         slavic=${foods.data.slavic}, romanian=${foods.data.romanian}, 
-        other=${foods.data.other}, french=${foods.data.french}, cookies=${foods.data.cookies}, french=${foods.data.french}, cookies=${foods.data.cookies} 
+        other=${foods.data.other}, french=${foods.data.french}, cookies=${foods.data.cookies} 
         WHERE userid=${foods.data.userid};`;
 
     return new Promise(function(resolve, reject) {
